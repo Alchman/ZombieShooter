a6cbb767e924ae022113c0950dbba0cfd9056984
Denis <alch007@gmail.com>
2020-12-08 21:02:39 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(inputX, inputY);
+
+        if (direction.magnitude > 1)
+        {
+            direction = direction.normalized;
+        }
+
+        rb.velocity = direction * speed;
+    }
+
+    void Rotate()
+    {
+        Vector3 playerPosition = transform.position;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = mousePosition - playerPosition;
+        direction.z = 0;
+        transform.up = -direction;
+    }
+}
